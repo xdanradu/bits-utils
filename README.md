73d7f07dc1e1d1f608e7164d72ea17f9562997a0
Dan Radu <xdanradu@yahoo.com>
2021-04-04 17:32:36 +0300

    Up

---
+++ README.md
@@ -0,0 +1,30 @@
+# Bits handling utilities
+
+```js
+
+var bitsUtils = require("bits-utils");
+let buffer = new Uint8Array(1);
+bitsUtils.setBit(buffer, 0, 0, 1)
+bitsUtils.setBit(buffer, 0, 1, 0)
+bitsUtils.setBit(buffer, 0, 2, 1)
+
+console.log(
+    bitsUtils.readBit(buffer, 0, 0),  bitsUtils.readBit(buffer, 0, 1),  bitsUtils.readBit(buffer, 0, 2)
+);
+
+```
+
+```js
+console.log(countDistinctSymbols('AAAA B C D D D D').map((el,index) => {return {symbol: String.fromCharCode(index), count: el}}));
+```
+
+## Which process listenes to port 4200
+```bash
+netstat -ano | findStr "4200"
+```
+
+
+## Kill a specific PID
+```bash
+Taskkill /PID 25740 /F
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+function readBit(buffer, i, bit) {
+  return (buffer[i] >> bit) % 2;
+}
+
+function setBit(buffer, i, bit, value) {
+  if(value == 0) {
+    buffer[i] &= ~(1 << bit);
+  } else{
+    buffer[i] |= (1 << bit);
+  }
+}
+
+function countDistinctASCIISymbols(text) {
+  let counter = Array(256).fill(0);
+
+  for (let i=0; i< text.length; i++) {
+    counter[text[i].charCodeAt(0)]++;
+  }
+
+  return counter.map((el,index) => { return {symbol: String.fromCharCode(index), count: el } });
+}
+
+function getSize(bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+module.exports = {
+  readBit,
+  setBit,
+  countDistinctASCIISymbols,
+  getSize
+}
